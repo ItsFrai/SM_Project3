32272067ef4955caeb9e36e25e57158eeed206da
SCooldude <sammanpandey7@gmail.com>
2023-10-31 01:42:30 -0400

    Initialized complete

--- src/main/java/com/example/rubank_ui/HelloApplication.java
+++ src/main/java/com/example/rubank_ui/HelloApplication.java
@@ -6,29 +6,43 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application implements EventHandler<ActionEvent> {
+public class HelloApplication extends Application  {
     @Override
     public void start(Stage stage) throws IOException {
+
+        /* Button openButton =new Button("Open");
+         openButton.setOnAction(new OpenButtonHandler());*/
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Project 3 - Transaction Manager");
         stage.setScene(scene);
         stage.show();
+
+
+
+
+
     }
 
-    @Override
-    public void handle(ActionEvent event){
 
-        if (event.getSource()==open){
-            System.out.print("Lets go");
-        }
-    }
 
     public static void main(String[] args) {
         launch();
     }
+ class OpenButtonHandler implements EventHandler <ActionEvent>{
+
+     @Override
+     public void handle(ActionEvent event) {
+         System.out.println("lets go");
+
+     }
+ }
+
 }
--- src/main/java/com/example/rubank_ui/HelloController.java
+++ src/main/java/com/example/rubank_ui/HelloController.java
@@ -2,15 +2,33 @@ package com.example.rubank_ui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
+
 
 public class HelloController {
 
         @FXML
         private ToggleGroup Campus;
+
         @FXML
-        public void handleCollegeCheckingSelection() {
+        private TextArea mainOutput;
+
+        @FXML
+        private Label firstNameLabel;
+
+        @FXML
+        private Label lastNameLabel;
+
+        @FXML
+        private DatePicker DOBLabel;
+
+        @FXML
+        private Label AmountLabel;
+
+        @FXML
+        private Button openButton;
+        @FXML
+        void handleCollegeCheckingSelection(ActionEvent event) {
                 // Enable the Campus radio buttons
                 Campus.getToggles().forEach(toggle -> {
                         if (toggle instanceof RadioButton) {
@@ -19,7 +37,7 @@ public class HelloController {
                 });
         }
         @FXML
-        public void handleOtherAccountSelection() {
+        void handleOtherAccountSelection(ActionEvent event) {
                 Campus.getToggles().forEach(toggle -> {
                         if (toggle instanceof RadioButton) {
                                 ((RadioButton) toggle).setDisable(true);
@@ -27,9 +45,4 @@ public class HelloController {
                 });
         }
 
-        public void handleOpenAccount(ActionEvent event){
-
-
-
-        }
 }
